a5d8d78441a9abed4e5f157c841722b48f7201fa
Akanimo <Akanumoh4@gmail.com>
2023-04-14 20:02:27 +0100

    News Homepage

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 document.getElementById("open").onclick = function() {
     document.getElementById("dropdown").style.right = "0%";
-    window.body.style.background = rgba(0, 0, 0, 0.4);
+    // document.getElementById("dropdown").style.opacity = "1";
+    const body = document.querySelector('body');
+    const nav = document.querySelector('nav');
+    const dropdown = document.querySelector('#dropdown');
+    const logo = document.querySelector('.logo');
+    
+    for (let i = 0; i < body.children.length; i++) {
+      if (body.children[i] !== nav && body.children[i] !== dropdown) {
+        body.children[i].style.opacity = 0.5;
+      }
+    }
+    
+    nav.style.opacity = 1;
+    logo.style.opacity = 0.5;
+    dropdown.style.opacity = 1;
+    dropdown.style.zIndex = 999;
 }
 
 document.getElementById("close").onclick = function() {
     document.getElementById("dropdown").style.right = "-100%";
+    
+    const body = document.querySelector('body');
+    const logo = document.querySelector('.logo');
+
+    for (let i = 0; i < body.children.length; i++) {
+        body.children[i].style.opacity = 1;
+    }      
+
+    logo.style.opacity = 1;
 }
